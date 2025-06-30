edd4a7771c55acda8e0d766b8b5bbbc20c0329e4
Fukano Masato <marbo0805@gmail.com>
2025-06-30 15:58:20 +0900

    Initial Commit

---
+++ PhotoMaster_swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoMaster_swiftui
+//
+//  Created by 深野真人 on 2025/06/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoMaster_swiftui/PhotoMaster_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMaster_swiftuiApp.swift
+//  PhotoMaster_swiftui
+//
+//  Created by 深野真人 on 2025/06/30.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMaster_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
